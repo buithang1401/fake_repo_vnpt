708ce9c366542eb9f0f4bd0470983abf36496969
buithang1401 <thang.dev.1401@gmail.com>
2022-06-10 16:11:41 +0700

    ok

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-This file is a fake cofig
+DATABASES = {
+    'default': {
+        'username': "asdfgjgagh",
+        'password': "1234fd8wtghTD",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 username = "adminforvnpt"
 password = "asfsdf124dftgh"
